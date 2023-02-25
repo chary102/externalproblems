980b57902d8747c2747f9036fcc200979976c45b
chary102 <113624749+chary102@users.noreply.github.com>
2023-02-25 15:01:00 +0530

    Update re.py

--- re.py
+++ re.py
@@ -1,7 +1,12 @@
+import time
+t=time.time()
 def rev(s):
     if len(s) == 0:
         return s
     else:
         return rev(s[1:])+ s[0]
 a = input("Enter the string: ")
-print(rev(a))
+y=rev(a)
+print(y)
+q=time.time()
+print(q-t)
